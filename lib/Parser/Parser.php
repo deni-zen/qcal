7eedc5a5a04698a260cca5ce7af006dff05b40ba
Luke Visinoni <luke@Lukes-iMac.local>
2014-02-15 01:19:34 -0800

    Committed parser

---
+++ lib/Parser/Parser.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * iCalendar Abstract Parser Class
+ *
+ * @author      Luke Visinoni <luke.visinoni@gmail.com>
+ * @copyright   (c) 2014 Luke Visinoni <luke.visinoni@gmail.com>
+ * @license     GNU Lesser General Public License v3 (see LICENSE file)
+ * @todo        The "Parser" namespace is going to be problematic. Maybe change
+ *              it ti "Parse" or something.
+ */
+namespace qCal\Parser;
+
+class Parser {
+
+    public function __construct() {
+    
+        
+    
+    }
+
+}
