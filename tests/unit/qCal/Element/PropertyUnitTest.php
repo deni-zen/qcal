26d2390dab58859b21962370d92e68cda1948443
Luke Visinoni <luke.visinoni@gmail.com>
2014-02-23 23:50:23 -0800

    Fixed property conflict

--- tests/unit/qCal/Element/PropertyUnitTest.php
+++ tests/unit/qCal/Element/PropertyUnitTest.php
@@ -1,18 +1,5 @@
 <?php
 /**
-<<<<<<< HEAD
- * qCal\Element\Component unit tests
- */
-namespace qCal\UnitTest\Element;
-use \qCal\Element\Property;
-
-class PropertyUnitTest extends \qCal\UnitTest\TestCase {
-
-    public function testCoreICalComponent() {
-    
-        $cal = new Component\VCalendar();
-        $this->assertEqual($cal->getName(), 'VCALENDAR');
-=======
  * qCal\Element\Property unit tests
  */
 namespace qCal\UnitTest\Element;
@@ -26,7 +13,6 @@ class PropertyUnitTest extends \qCal\UnitTest\TestCase {
         $prop = new Property\Action('AUDIO');
         $this->assertIsA($prop->getValue(), 'qCal\Value\Text');
         $this->assertEqual($prop->getValue(), new Value\Text('AUDIO'));
->>>>>>> 80060ca0afd8344ce20d3ca0f283c7b15e363172
     
     }
 
